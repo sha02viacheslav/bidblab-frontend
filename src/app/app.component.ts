508bf11f6f559e6f0c3ba0370acdc8d8de35a5f7
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-05-20 13:41:05 +0300

    conbine jquery nicescroll and infinite scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { AuthenticationService } from './shared/services/authentication.service'
 import { DialogService } from './shared/services/dialog.service';
 // import { ResetPasswordComponent } from './shared/components/reset-password/reset-password.component';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
-// declare var $: any;
+declare var $: any;
 
 // import { LoginComponent } from './shared/components/login/login.component';
 // import { SignupDialogComponent } from './shared/components/signup-dialog/signup-dialog.component';
@@ -85,20 +85,20 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    // $("#main-nicescrollable").niceScroll({
-		// 	cursorcolor: "#e91e63",
-		// 	cursorborder: '#e91e63',
-		// 	autohidemode: true,
-		// 	background: "#aaa",
-		// 	cursorminheight: 15,
-		// 	cursorborderradius: 15,
-		// 	cursorwidth: 3,
-		// 	cursoropacitymin: 0.1,
-    // });
+    $("#main-nicescrollable").niceScroll({
+			cursorcolor: "#e91e63",
+			cursorborder: '#e91e63',
+			autohidemode: true,
+			background: "#aaa",
+			cursorminheight: 15,
+			cursorborderradius: 15,
+			cursorwidth: 3,
+			cursoropacitymin: 0.1,
+    });
 
-    // setInterval(() => {
-    //   $("#main-nicescrollable").getNiceScroll().resize();
-    // }, 100);
+    setInterval(() => {
+      $("#main-nicescrollable").getNiceScroll().resize();
+    }, 100);
     
     this.menuItems = this.menuService.getVerticalMenuItems();
     this.getBlockStatus();
