78bec4e0af841d692e4996173e1e494be8859b32
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-05-23 02:49:52 +0300

    bid result with table

--- src/app/shared/components/bid-list/auction-result/auction-result.component.ts
+++ src/app/shared/components/bid-list/auction-result/auction-result.component.ts
@@ -1,76 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { AlertDialogComponent } from '../../../../shared/components/alert-dialog/alert-dialog.component';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { CommonService } from '../../../shared/services/common.service';
+import { HttpErrorResponse } from '@angular/common/http';
+// import { AuthenticationService } from '../../../shared/services/authentication.service';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { environment } from '../../../../../environments/environment';
-import { BidService } from '../bid.service';
-import { AuctionDialogComponent } from '../auction-dialog/auction-dialog.component';
-import { MatDialog } from '@angular/material';
+// import { MatDialog } from '@angular/material';
+import { MatTableDataSource, MatOption, MatSort } from '@angular/material';
 
 @Component({
   selector: 'app-auction-result',
   templateUrl: './auction-result.component.html',
   styleUrls: ['./auction-result.component.scss']
 })
-export class AuctionResultComponent implements OnInit {
+export class AuctionResultComponent implements OnInit, AfterViewInit {
 
   @Input() auction: any;
   @Input() auctionType: any;
   private serverUrl = environment.apiUrl;
+	public displayedColumns: string[] = ['bidPrice', 'username', 'createdAt'];
+	public dataSource:any;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
-    private bidService: BidService,
-		public dialog: MatDialog,
+    // private bidService: BidService,
   ) {
   }
 
   ngOnInit() {
-  }
-
-  openBidDialog(){
-    if(this.bidService.availuableCredits < this.auction.bidFee){
-      this.dialog.open(AlertDialogComponent, 
-        {
-          data: {
-            title: "You need more BidBlab Credits to continue bidding!",
-            comment: "You can earn more BibBlab Credits by submitting and answering questions",
-            dialog_type: "alert" 
-          },
-          width: '320px',
-        });
-    }
-    else{
-      this.dialog
-        .open(AuctionDialogComponent, {
-          data: {
-            auction: this.auction
-          },
-          width: '800px'
-        })
-        .afterClosed()
-        .subscribe(newBid => {
-          if (newBid) {
-            this.dialog.
-              open(AlertDialogComponent, {
-                data: {
-                  title: "Bid successfully submitted!",
-                  comment: "",
-                  dialog_type: "bid" 
-                },
-                width: '320px',
-              }).afterClosed().subscribe(result => {
-                if(result == 'more'){
-                  this.openBidDialog();
-                }
-              });
-          }
-        });
-    }
-  }
-
-  displayDetail(){
-    this.bidService.detailAuction = this.auction;
-    this.bidService.detailAuction.display = true;
+    this.auction.bids.forEach(element => {
+      element.username = element.bidder.username;
+    });
+    this.dataSource = new MatTableDataSource<any>(this.auction.bids);
   }
   
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  
 }
+
+
+
 
 
