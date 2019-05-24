a64479cffff9003c7b61197e4db73fdf963e58bf
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-05-24 18:59:33 +0300

    add submitted variable in invite component

--- src/app/account/invite/invite.component.ts
+++ src/app/account/invite/invite.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router, ActivatedRoute  } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
 import { AuthenticationService } from '../../shared/services/authentication.service';
-import { MatSnackBar, MatDialogRef } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 import { CommonService } from '../../shared/services/common.service';
+import { BlockUIService } from '../../shared/services/block-ui.service';
 
 @Component({
   selector: 'app-invite',
@@ -12,14 +12,14 @@ import { CommonService } from '../../shared/services/common.service';
   styleUrls: ['./invite.component.scss']
 })
 export class InviteComponent implements OnInit{
-  public form:FormGroup;
-	public passwordHide:boolean = true;
-  returnUrl: string = '';
+  public form: FormGroup;
   public defaultCredits: any;
+  public submitted: boolean = false;
 
   constructor(
     public fb: FormBuilder,
     public router:Router,
+    private blockUIService: BlockUIService,
     private authenticationService: AuthenticationService,
     public commonService: CommonService,
     private route: ActivatedRoute,
@@ -31,34 +31,27 @@ export class InviteComponent implements OnInit{
   };
 
   ngOnInit(){
-    this.commonService.getDefaultCredits().subscribe(
-			(res: any) => {
-					this.defaultCredits = res.data;
-				},
-			(err: HttpErrorResponse) => {
-				}
-			);
+    this.blockUIService.setBlockStatus(true);
+    this.commonService.getDefaultCredits().subscribe((res: any) => {
+      this.defaultCredits = res.data;
+      this.blockUIService.setBlockStatus(false);
+    });
   }
 
   public submitForm() {
     if (this.form.valid) {
-      this.commonService.invite(this.form.value).subscribe(
-        (res: any) => {
-          if(res.data){
-            this.snackBar.open(res.msg, 'Dismiss', {
-              duration: 1500
-            });
-            this.form.reset();
-          }
-          else{
-            this.snackBar.open(res.msg, 'Dismiss', {
-              duration: 1500
-            });
-          }
-        },
-        (err: HttpErrorResponse) => {
+      this.submitted = true;
+      this.blockUIService.setBlockStatus(true);
+      this.commonService.invite(this.form.value).subscribe((res: any) => {
+        this.blockUIService.setBlockStatus(false);
+        this.submitted = false;
+        if(res.data) {
+          this.snackBar.open(res.msg, 'Dismiss', {duration: 1500});
+          this.form.reset();
+        } else {
+          this.snackBar.open(res.msg, 'Dismiss', {duration: 1500});
         }
-      );  
+      });  
     }
   }
 
