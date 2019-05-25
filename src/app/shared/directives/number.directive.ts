73728ef30454afb613b8a7677e886e388f048f68
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-05-25 18:27:23 +0300

    bid price dirction

--- src/app/shared/directives/number.directive.ts
+++ src/app/shared/directives/number.directive.ts
@@ -21,8 +21,7 @@ export class NumericDirective implements OnInit{
 
     private regex = {
         number: new RegExp(/^\d+$/),
-        decimal: new RegExp(/^\d*\.?\d{0,2}$/g),
-        mobiledecimal: new RegExp(/^\d*\.?\d{0,2}$/g)
+        decimal: new RegExp(/^\d*\.?\d{0,2}$/g)
         // decimal: new RegExp(/^[0-9]+(\.[0-9]*){0,1}$/g)
     };
 
@@ -37,84 +36,39 @@ export class NumericDirective implements OnInit{
     ) { }
 
     ngOnInit(){
-        if(this.numericType == 'mobiledecimal'){
-            this.el.nativeElement.value = '0.00';
-            this.cntKeyPress = 0;
-        }
+        this.el.nativeElement.value = '0.00';
     }
 
     @HostListener('keydown', [ '$event' ])
     onKeyDown(event: KeyboardEvent) {
-
-        if(this.numericType == 'mobiledecimal'){
-            console.log(this.cntKeyPress);
-            var temp;
-            var keyTemp = String(event.key);
-            if((event.keyCode == this.desktopKeyCode.Enter 
-                || event.keyCode == this.mobileKeyCode.Enter) 
-                && this.cntKeyPress > 0){
-                return;
-            }
-            else if((event.keyCode == this.desktopKeyCode.Backspace 
-                || event.keyCode == this.mobileKeyCode.Backspace) 
-                && this.cntKeyPress > 0){
-                switch (this.cntKeyPress){
-                    case(1):{ 
-                        temp = '0.00';
-                        break;
-                    }
-                    case(2):{ 
-                        temp = '0.0' + this.el.nativeElement.value.slice(-1);
-                        break;
-                    }
-                    case(3):{ 
-                        temp = '0.' + this.el.nativeElement.value.slice(-2);
-                        break;
-                    }
-                    default: {
-                        temp = this.el.nativeElement.value.slice(1);
-                        break;
-                    }
-                }
-                this.cntKeyPress--;
-                setTimeout(() => { 
-                    this.control.control.setValue(temp);
-                }, 100);
-                
-            }
-            else if(keyTemp.match(new RegExp(/^[a-zA-Z]*\d+$/)) != null){
-                switch (this.cntKeyPress){
-                    case(0):{ temp = '0.0' + keyTemp; break;}
-                    case(1):{ 
-                        temp = '0.' + keyTemp + this.el.nativeElement.value.slice(-1);
-                        break;
-                    }
-                    case(2):{ 
-                        temp = keyTemp + this.el.nativeElement.value.slice(-3);
-                        break;
-                    }
-                    default: {
-                        temp = keyTemp + this.el.nativeElement.value;
-                        break;
-                    }
-                }
-                this.cntKeyPress++;
-                
+        var temp;
+        var keyTemp = String(event.key);
+        if(event.keyCode == this.desktopKeyCode.Enter || event.keyCode == this.mobileKeyCode.Enter){
+            return;
+        }
+        else if(event.keyCode == this.desktopKeyCode.Backspace || event.keyCode == this.mobileKeyCode.Backspace){
+            setTimeout(() => {
+                temp = (Math.floor(Number(this.el.nativeElement.value) * 10)/100).toFixed(2);
                 this.control.control.setValue(temp);
-            }
-            event.preventDefault();
+            }, 100);
         }
-        else{
-            if (this.specialKeys[this.numericType].indexOf(event.key) !== -1) {
-                return;
-            }
-            // Do not use event.keycode this is deprecated.
-            // See: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
-            let current: string = this.el.nativeElement.value;
-            let next: string = current.concat(event.key);
-            if (next && !(next).match(this.regex[this.numericType])) {
-                event.preventDefault();
-            }
+        else if(keyTemp.match(new RegExp(/^[a-zA-Z]*\d+$/)) != null){
+            temp = ((Number(this.el.nativeElement.value) * 1000 + Number(keyTemp)) / 100).toFixed(2);
+            this.control.control.setValue(temp);
         }
+        event.preventDefault();
+        // }
+        // else{
+        //     if (this.specialKeys[this.numericType].indexOf(event.key) !== -1) {
+        //         return;
+        //     }
+        //     // Do not use event.keycode this is deprecated.
+        //     // See: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+        //     let current: string = this.el.nativeElement.value;
+        //     let next: string = current.concat(event.key);
+        //     if (next && !(next).match(this.regex[this.numericType])) {
+        //         event.preventDefault();
+        //     }
+        // }
     }
 }
