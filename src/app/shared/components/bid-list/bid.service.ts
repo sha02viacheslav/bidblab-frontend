d5fc54bc0a9f7d3236e54d4d20e12743200d246c
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-06-25 01:40:10 +0300

    sign up credit in bid page

--- src/app/shared/components/bid-list/bid.service.ts
+++ src/app/shared/components/bid-list/bid.service.ts
@@ -18,6 +18,7 @@ export class BidService {
   public optionalImageCredits: number = 0;
   public answerCredits: number = 0;
   public referalCredits: number = 0;
+  public signupCredits: number = 0;
   public totalCredits: number = 0;
   public loseCredits: number = 0;
   public availuableCredits: number = 0;
@@ -33,7 +34,8 @@ export class BidService {
         this.optionalImageCredits = res.data.credits.optionalImageCredits;
         this.answerCredits = res.data.credits.answerCredits;
         this.referalCredits = res.data.credits.referalCredits;
-        this.totalCredits = this.questionCredits + this.optionalImageCredits + this.answerCredits + this.referalCredits;
+        this.signupCredits = res.data.credits.signupCredits;
+        this.totalCredits = this.questionCredits + this.optionalImageCredits + this.answerCredits + this.referalCredits + this.signupCredits;
         this.loseCredits = res.data.credits.loseCredits;
         this.availuableCredits = this.totalCredits - this.loseCredits;
       },
