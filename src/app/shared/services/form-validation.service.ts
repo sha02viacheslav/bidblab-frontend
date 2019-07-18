339b412b42de4991be25087211fa4997dbf24d2e
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-07-18 21:15:59 +0300

    adault fix

--- src/app/shared/services/form-validation.service.ts
+++ src/app/shared/services/form-validation.service.ts
@@ -2,49 +2,60 @@ import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class FormValidationService {
-  isBlank(control: FormControl) {
-    return control && control.value && control.value.trim()
-      ? null
-      : { blank: true };
-  }
+	isBlank(control: FormControl) {
+		return control && control.value && control.value.trim()
+			? null
+			: { blank: true };
+	}
 
-  isAdault(control: FormControl) {
-    if(control && control.value){
-      var ageDifMs = Date.now() - control.value.getTime();
-      var ageDate = new Date(ageDifMs); // miliseconds from epoch
-      var age = Math.abs(ageDate.getUTCFullYear() - 1970);
-      if( age >= 18){
-        return null;
-      }
-    }
-    return { notAdault: true };
-  }
+	isAdault(control: FormControl) {
+		if (control && control.value) {
+			var nowDate = new Date();
+			var birthDate = control.value;
+			var yearDif = nowDate.getFullYear() - birthDate.getFullYear();
+			if (yearDif < 18) {
+				return { notAdault: true };
+			} else if (yearDif == 18) {
+				var monthDif = nowDate.getMonth() - birthDate.getMonth();
+				if (monthDif < 0) {
+					return { notAdault: true };
+				} else if (monthDif == 0) {
+					var dayDif = nowDate.getDate() - birthDate.getDate();
+					if (dayDif < 0) {
+						return { notAdault: true };
+					}
+				}
+			}
+			return false;
+		}
+		return { notAdault: true };
+	}
 
-  arePasswordsMismatching(control: FormControl) {
-    return control.value &&
-      control.parent &&
-      control.value === control.parent.get('password').value
-      ? null
-      : {
-          passwordsMismatch: true
-        };
-  }
+	arePasswordsMismatching(control: FormControl) {
+		return control.value &&
+			control.parent &&
+			control.value === control.parent.get('password').value
+			? null
+			: {
+				passwordsMismatch: true
+			};
+	}
 
-  checkError(form, field, error) {
-    if (Array.isArray(error)) {
-      return error.some(
-        err =>
-          form.get(field).hasError(err) &&
-          (form.get(field).dirty || form.get(field).touched)
-      );
-    } else {
-      return (
-        form.get(field).hasError(error) &&
-        (form.get(field).dirty || form.get(field).touched)
-      );
-    }
-  }
+	checkError(form, field, error) {
+		if (Array.isArray(error)) {
+			return error.some(
+				err =>
+					form.get(field).hasError(err) &&
+					(form.get(field).dirty || form.get(field).touched)
+			);
+		} else {
+			return (
+				form.get(field).hasError(error) &&
+				(form.get(field).dirty || form.get(field).touched)
+			);
+		}
+	}
 }
