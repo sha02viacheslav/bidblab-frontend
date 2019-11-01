ede31f58128c4313f473a5a5c9af7c31d787ed7d
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-11-01 12:28:06 +0300

    remove inline style in SSR

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -7,14 +7,29 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 
 import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 
+import { ɵangular_packages_platform_server_platform_server_c as ServerStylesHost } from '@angular/platform-server';
+
+export class NoRenderServerStylesHost extends ServerStylesHost {
+
+  onStylesAdded(additions: Set<string>): void {
+      // super.onStylesAdded(additions);
+      // additions.forEach((s) => console.log(s));
+      // ignore styles added
+  }
+}
+
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
     ServerTransferStateModule,
-    FlexLayoutServerModule
+    // FlexLayoutServerModule
   ],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: ServerStylesHost,
+    useClass: NoRenderServerStylesHost
+  }]
 })
 export class AppServerModule {}
