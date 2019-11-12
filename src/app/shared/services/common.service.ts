c2c25ea3a797fce5cd6d05d861f3cbdb4a44c2a6
Viacheslav Shatan <sha02viacheslav@gmail.com>
2019-11-12 20:58:27 +0300

    style in myquestion and myanswer

--- src/app/shared/services/common.service.ts
+++ src/app/shared/services/common.service.ts
@@ -117,10 +117,7 @@ export class CommonService {
   changeProfilePicture(body) {
     return this.httpClient.patch(
       `${environment.apiUrl}/api/auth/changeProfilePicture`,
-      body,
-      {
-        reportProgress: true,
-      }
+      body
     );
   }
 
